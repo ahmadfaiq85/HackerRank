1d73653e05a2e7ff45b47480e0239b9229be7c1f
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2017-11-09 09:59:25 -0500

    Fixed Super Reduced String

--- SuperReduceString/Program.cs
+++ SuperReduceString/Program.cs
@@ -4,29 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace SuperReducedString
 {
     class Program
     {
         static string super_reduced_string(string s)
         {
             int n = s.Length;
-            
+
             for (int i = 0; i <= n - 1; i++)
             {
+                if (i + 2 > n)
+                    break;
+
                 if (s.Substring(i, 1) == s.Substring(i + 1, 1))
                 {
                     s = s.Remove(i, 2);
-                    i -= 2;
-                } 
+                    i = -1;
+                    n = s.Length;
+                }
             }
-
 
             if (s.Length == 0)
                 return "Empty String";
             else
                 return s;
-           
         }
 
         static void Main(String[] args)
