e3993d61e77276e85b4c5f9dd6edc32a58381665
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-01-10 09:33:48 -0500

    Solved Balanced Brackets challenge

---
+++ 16TwoCharacters/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoCharacters
+{
+    class Program
+    {
+        public static void removeConsecutive(List<char> sArr)
+        {
+            for (int i = 0; i <= sArr.Count - 1; i++)
+            {
+                if (i != 0)
+                {
+                    if (sArr[i] == sArr[i - 1])
+                    {
+                        sArr.RemoveAll(a => a == sArr[i]);
+                        removeConsecutive(sArr);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public static void removeRemaining(List<char> charsToKeep, List<char> sArr)
+        {
+            foreach (var item in sArr)
+            {
+                if (!charsToKeep.Contains(item))
+                {
+                    sArr.RemoveAll(x => x == item);
+                    removeRemaining(charsToKeep, sArr);
+                    break;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int len = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+
+            var sArr = new List<char>(s.ToCharArray());
+
+            removeConsecutive(sArr);
+
+            var charsToKeepGroup = sArr.GroupBy(a => a).OrderByDescending(a => a.Count()).Take(2).ToList();
+
+            List<char> charsToKeep = new List<char>();
+
+            foreach(var charKeep in charsToKeepGroup)
+            {
+                charsToKeep.Add(Convert.ToChar(charKeep.ToArray().GetValue(0)));
+            }
+
+            removeRemaining(charsToKeep, sArr);
+
+            Console.WriteLine(sArr.Count());
+        }
+    }
+}
---
+++ 17BalancedBrackets/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                string s = Console.ReadLine();
+                string result = isBalanced(s);
+                Console.WriteLine(result);
+            }
+        }
+
+        static string isBalanced(string s)
+        {   
+            Stack<char> charStack = new Stack<char>();
+
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (s[i].Equals('(') || s[i].Equals('[') || s[i].Equals('{'))
+                    charStack.Push(s[i]);
+                else
+                {
+                    if (charStack.Count == 0)
+                        return "NO";
+
+                    if (charStack.Peek().Equals('(') && s[i].Equals(')') ||
+                        charStack.Peek().Equals('[') && s[i].Equals(']') ||
+                        charStack.Peek().Equals('{') && s[i].Equals('}')
+                        )
+                    {
+                        charStack.Pop();
+                    }
+                       
+                    
+                } 
+            }
+
+            if (charStack.Count == 0)
+                return "YES";
+            else
+                return "NO";
+        }
+
+    }
+}
