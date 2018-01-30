420d9291bd3f2e7ff76b5207fe6426d396dc8b7a
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-01-30 16:47:17 -0500

    Solved Mars Exploration challenge

---
+++ 19MarsExploration/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsExploration
+{ 
+    class Program
+    {
+        static int marsExploration(string s)
+        {
+            string sos = "SOS";
+            int isos = 0;
+
+            int errors = 0;
+            
+            for (int j = 0; j <= s.Length - 1; j++)
+            {
+                if (isos > 2)
+                    isos = 0;
+
+                if (s[j] != sos[isos])
+                    errors++;
+
+                isos++;
+            }
+
+            return errors;
+        }
+
+        static void Main(String[] args)
+        {
+            string s = Console.ReadLine();
+            int result = marsExploration(s);
+            Console.WriteLine(result);
+        }
+    }
+}
