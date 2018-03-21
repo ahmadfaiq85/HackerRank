b532e13c720e33705a687ffeccc6c9e44dff6fb5
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2018-03-21 11:23:43 -0400

    HackerRank LinkedList insert to head

---
+++ 24LinkedListInsertHead/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListInsertHead
+{
+    class Program
+    {
+        public class Node
+        {
+            public int Data;
+            public Node Next;
+
+        }
+
+        public static Node Insert(Node head, int x)
+        {
+            if (head == null)
+            {
+                head = new Node();
+                head.Data = x;
+                return head;
+            }
+            else
+            {
+                Node newHead = new Node();
+                newHead.Data = x;
+                newHead.Next = head;
+                return newHead;
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
